e422b5bb84bbd3d54be13439b4255121627971e6
bmitioglov <ibmttz13.5@gmail.com>
2014-04-15 00:39:08 +0400

    1 commit

---
+++ src/main/java/Constants.java
@@ -0,0 +1,19 @@
+
+public class Constants {
+    public static final String
+        LBRA = "{",
+        RBRA = "}",
+        EQUAL = "=",
+        SEMICOLON = ";",
+        LPAR = "(",
+        RPAR = ")",
+        PLUS = "+",
+        MINUS = "-",
+        LESS = "<",
+        IF = "if",
+        ELSE = "else",
+        DO = "do",
+        WHILE = "while";
+
+
+}
